b56ec7b83202c6956da00241cc574ba98d4f0fb6
calebacree <caleb.acree@gmail.com>
2024-09-11 21:57:10 -0500

    Update child.c and change name to user.c

--- child.c
+++ child.c
@@ -1,17 +1,34 @@
-#include<unistd.h>
-#include<sys/types.h>
-#include<stdio.h>
-#include<stdlib.h>
-int main(int argc, char** argv) {
-		printf("Hello from Child.c, a new executable!\n");
-		printf("My process id is: %d\n",getpid());
-		printf(" I got %d arguments: \n", argc);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-		int i;
-		for (i =0; i < argc; i++)
-				printf("|%s| ", argv[i]);
-		printf("\nChild is now ending.\n");
+int main(int argc, char *argv[]) {
+    // Ensure the correct number of arguments are provided
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
+        return 1;
+    }
 
-		sleep(3);
-		return EXIT_SUCCESS;
+    // Convert the command-line argument to an integer
+    int iterations = atoi(argv[1]);
+
+    // Validate the iterations argument
+    if (iterations <= 0) {
+        fprintf(stderr, "Error: Iterations must be a positive integer.\n");
+        return 1;
+    }
+
+    // Loop for the specified number of iterations
+    for (int i = 0; i < iterations; i++) {
+        // Print information before sleeping
+        printf("USER PID:%d PPID:%d Iteration:%d before sleeping\n", getpid(), getppid(), i + 1);
+
+        // Sleep for 1 second
+        sleep(1);
+
+        // Print information after sleeping
+        printf("USER PID:%d PPID:%d Iteration:%d after sleeping\n", getpid(), getppid(), i + 1);
+    }
+
+    return 0;
 }
